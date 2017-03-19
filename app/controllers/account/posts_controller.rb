97e7b42a93164cf68b704186a82010c7314691f7
yaoyizhi <email@yaoadou888>
2017-03-19 10:09:08 +0800

    l

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -4,4 +4,16 @@ class Account::PostsController < ApplicationController
     def index
         @posts = current_user.posts
     end
+
+    def edit
+        @group = Group.find(params[:group_id])
+        @post.group = @group
+        @post = Post.find(params[:id])
+    end
+
+    def destroy
+        @post = current_user.post
+        @post.detroy
+        redirect_to group_post_path(@group)
+    end
 end
